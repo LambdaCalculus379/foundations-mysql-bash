3fc09a2b3fea97206826490e667a92f03b3a3041
Robert Menes <viewtiful.icchan@gmail.com>
2017-01-15 16:15:17 -0500

    Shell script to generate and test bash functions

---
+++ generate-functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Last known working good
+# $helloIn() { echo 'Hello Back!';}
+
+FUNC_NAME='helloIn'
+FUNC_STMT=$(echo 'HelloYourself')
+FUNC_OUTPUT='HelloYourself'
+FUNC_DEF=${FUNC_NAME}"() { echo ${FUNC_OUTPUT};}"
+
+echo $FUNC_DEF
+
+# Generated function definition string failed
+# $ helloIn() { echo Hello Yourself!;}
